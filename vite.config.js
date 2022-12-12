48e0b8f58295288d87821008fe6a449e72028dc9
vladislav.rassokhin <vladislav.rassokhin@theunit.dev>
2022-12-12 22:01:37 +0300

    feat: setup ESlint and prettier

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({command, mode}) => {
-    const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-    return {
-        define: {
-            VITE_BASE_API_URL: String(env.VITE_BASE_API_URL),
-        },
-        plugins: [react()],
-    }
+  return {
+    define: {
+      VITE_BASE_API_URL: String(env.VITE_BASE_API_URL)
+    },
+    plugins: [react()]
+  };
 });
