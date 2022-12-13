0bcb91c62c3354c5a9f8d1da62f4393dd8d44419
vladislav.rassokhin <vladislav.rassokhin@theunit.dev>
2022-12-14 00:03:33 +0300

    feat: add react router 6

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({command, mode}) => {
+    loadEnv(mode, process.cwd(), '');
 
-  return {
-    define: {
-      VITE_BASE_API_URL: String(env.VITE_BASE_API_URL)
-    },
-    plugins: [react()]
-  };
+    return {
+        plugins: [react()]
+    };
 });
