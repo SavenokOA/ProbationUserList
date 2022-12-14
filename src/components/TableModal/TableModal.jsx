49d16f362e8dd2b5c581d7bdb142423c315a2097
vladislav.rassokhin <vladislav.rassokhin@theunit.dev>
2022-12-15 02:27:54 +0300

    feat: create modal for editing user data

---
+++ src/components/TableModal/TableModal.jsx
@@ -0,0 +1,67 @@
+import React, { useCallback, useState } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+  TextField
+} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit.js';
+
+export const TableModal = ({ isOpen, setOpen, username, email, address, phone }) => {
+  const [isUsername, setUsername] = useState(username);
+  const [isEmail, setEmail] = useState(email);
+  const [isAddress, setAddress] = useState(address);
+  const [isPhone, setPhone] = useState(phone);
+
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  const handleEdit = () => {};
+
+  return (
+    <Dialog title="Edit data" open={isOpen}>
+      <DialogTitle>{'Edit'}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">Change user data</DialogContentText>
+        <TextField
+          value={isUsername}
+          label="username"
+          fullWidth
+          margin="normal"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          value={isEmail}
+          label="email"
+          fullWidth
+          margin="normal"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          value={isAddress}
+          label="address"
+          fullWidth
+          margin="normal"
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <TextField
+          value={isPhone}
+          label="phone"
+          fullWidth
+          margin="normal"
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+        <Button onClick={handleEdit} variant="outlined" startIcon={<EditIcon />}>
+          Edit
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
--- src/components/TableRow/TableRow.jsx
+++ src/components/TableRow/TableRow.jsx
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TableCell, Checkbox, Button, TableRow as MuiTableRow } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
+import { TableModal } from '../TableModal/TableModal';
 
-export const TableRow = ({ handleClick, isItemSelected, row, labelId }) => (
-  <MuiTableRow hover aria-checked={isItemSelected} tabIndex={-1} selected={isItemSelected}>
-    <TableCell padding="checkbox">
-      <Checkbox
-        role="checkbox"
-        onClick={(event) => handleClick(event, row.name)}
-        color="primary"
-        checked={isItemSelected}
-        inputProps={{
-          'aria-labelledby': labelId
-        }}
+export const TableRow = ({ handleClick, isItemSelected, row, labelId }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  return (
+    <MuiTableRow hover aria-checked={isItemSelected} tabIndex={-1} selected={isItemSelected}>
+      <TableCell padding="checkbox">
+        <Checkbox
+          role="checkbox"
+          onClick={(event) => handleClick(event, row.name)}
+          color="primary"
+          checked={isItemSelected}
+          inputProps={{
+            'aria-labelledby': labelId
+          }}
+        />
+      </TableCell>
+      <TableCell component="th" id={labelId} scope="row" padding="none">
+        {row.username}
+      </TableCell>
+      <TableCell align="left">{row.email}</TableCell>
+      <TableCell align="left">{row.address.street}</TableCell>
+      <TableCell align="right">{row.phone}</TableCell>
+      <TableCell align="right">
+        <Button variant="outlined" startIcon={<EditIcon />} onClick={() => setModalOpen(true)}>
+          Edit
+        </Button>
+      </TableCell>
+      <TableModal
+        isOpen={isModalOpen}
+        setOpen={setModalOpen}
+        username={row.username}
+        email={row.email}
+        address={row.address.street}
+        phone={row.phone}
       />
-    </TableCell>
-    <TableCell component="th" id={labelId} scope="row" padding="none">
-      {row.username}
-    </TableCell>
-    <TableCell align="left">{row.email}</TableCell>
-    <TableCell align="left">
-      {row.address.city} {row.address.street}
-    </TableCell>
-    <TableCell align="right">{row.phone}</TableCell>
-    <TableCell align="right">
-      <Button color="secondary" variant="outlined" startIcon={<EditIcon />}>
-        Edit
-      </Button>
-    </TableCell>
-  </MuiTableRow>
-);
+    </MuiTableRow>
+  );
+};
