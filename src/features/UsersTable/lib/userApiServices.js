0f2cfb2c7d79ed08b26617cf5419af220ab1466f
vladislav.rassokhin <vladislav.rassokhin@theunit.dev>
2022-12-14 08:58:01 +0300

    feat: setup json server with user data in db.json file

--- src/features/UsersTable/lib/userApiServices.js
+++ src/features/UsersTable/lib/userApiServices.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const userApiServices = createApi({
   reducerPath: 'userApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: (import.meta.env.VITE_BASE_API_URL) }),
   endpoints: (builder) => ({
     getAllUsers: builder.query({
       query: () => `/users`
